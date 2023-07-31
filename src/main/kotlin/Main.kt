fe9d4d01ad376a94f52b7defc06cb4e83c559710
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-07-31 11:52:38 +0300

    Second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 fun main() {
-    val likes = 111
+    val likes = 3
 
-    if (likes % 10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+    if (likes % 100 == 11) {
+        println("Понравилось $likes людям")
+    } else if (likes % 10 == 1) {
+        println("Понравилось $likes человеку")
+    } else println("Понравилось $likes людям")
 }
